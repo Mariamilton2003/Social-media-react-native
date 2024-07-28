3b09515e9f8c082aae1412997a25a2d8336ba798
Mariamilton2003 <miltonmaria732@gmail.com>
2024-07-28 22:33:55 +0530

    bug fixed

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './src/services/store';
+import {store} from './src/services/store';
 import AppNavigator from './src/navigation/AppNavigator';
 
 const App = () => {
